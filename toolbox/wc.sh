8147fbebe025b00d6a5d65803497d83d66a2ba89
witiko <witiko@gmail.com>
2015-05-07 00:45:10 +0200

    Added Chapter 2 and restructuded Section 3.2.

--- toolbox/wc.sh
+++ toolbox/wc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# Prints the approximate word count of the thesis.
-echo $(((`cat chapters/*.tex chapters/*/*.tex | wc -c | tail -n 1` + `pdf2txt print.pdf | wc -c`) / 2))
+# Prints the approximate word count of the thesis / 1800.
+echo $(((`cat print.tex | wc -c | tail -n 1` + `pdf2txt print.pdf |
+wc -c`) / (2*1800)))
