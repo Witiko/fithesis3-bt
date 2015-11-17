dba8eb8090c782d524df2a02b59a267a1c7cc817
Vít Novotný <witiko@gmail.com>
2015-11-17 23:52:24 +0100

    Added the latest revision of the technical documentation and guides.

--- misc/guide.sh
+++ misc/guide.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 typeset() {
-  sed -i 's/{[0123456789]*%??page??/{'$2'%??page??/' guide.tex
+  sed -i 's/{[0123456789]*%??page??/{'$2'%??page??/' guide.dtx
   make $1.pdf
 }
 
-make -C examples
+make -C resources
 typeset fi 33
-typeset sci 49
-typeset econ 65
-typeset fss 81
-typeset law 97
-typeset med 113
-typeset ped 129
-typeset phil 145
-typeset fsps 161
+typeset sci 53
+typeset econ 73
+typeset fss 91
+typeset law 111
+typeset med 131
+typeset ped 147
+typeset phil 167
+typeset fsps 187
 make clean
